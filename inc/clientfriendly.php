313f6b6b8634f7b04bff92d0673af9bb47d1a6cc
julian-weiland <julianweiland@icloud.com>
2015-05-03 00:20:21 +0200

    unregister unused sidebar widgets

--- inc/clientfriendly.php
+++ inc/clientfriendly.php
@@ -120,3 +120,22 @@ function fastwp_filter_query( $query, $error = true ) {
 
 add_action( 'parse_query', 'fastwp_filter_query' );
 add_filter( 'get_search_form', create_function( '$a', "return null;" ) );
+
+
+//Widgetd deaktivieren
+// unregister all default WP Widgets 
+function unregister_default_wp_widgets() {
+	unregister_widget('WP_Widget_Pages');
+	unregister_widget('WP_Widget_Calendar');
+	unregister_widget('WP_Widget_Archives');
+	unregister_widget('WP_Widget_Links');
+	unregister_widget('WP_Widget_Meta');
+	unregister_widget('WP_Widget_Search');
+	unregister_widget('WP_Widget_Text');
+	unregister_widget('WP_Widget_Categories');
+	unregister_widget('WP_Widget_Recent_Posts');
+	unregister_widget('WP_Widget_Recent_Comments');
+	unregister_widget('WP_Widget_RSS');
+	unregister_widget('WP_Widget_Tag_Cloud');
+}
+add_action('widgets_init', 'unregister_default_wp_widgets', 1);
