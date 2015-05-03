53530d52e6f5b03a1cab28e25586158d715d3fc9
julian-weiland <julianweiland@icloud.com>
2015-05-03 17:55:19 +0200

    Aside, svg background, removed Emoji
    Removed unused header from content-adside.php
    Added svg background to technik and footer.
    Removed Emoji script from header

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -219,6 +219,10 @@ function filter_ptags_on_images($content) //remove p-tags from imagesavealpha(im
 	}
 add_filter('the_content', 'filter_ptags_on_images');
 
-
+//Remove emoji js from header
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
 
 ?>
